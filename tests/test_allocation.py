165439b2237b814f24cee68cb685027863d7a344
TheBabelDragon <sphg5nyvyr@privaterelay.appleid.com>
2026-09-24 18:23:41 -0600

    Add allocation tests

---
+++ tests/test_allocation.py
@@ -0,0 +1,40 @@
+"""Cost allocation determinism tests."""
+
+from decimal import Decimal
+
+from freightflow.domain.shipment import Shipment
+from freightflow.domain.allocation import AllocationPolicy
+from freightflow.allocation.cost_allocator import CostAllocator
+
+
+def _shipments():
+    return [
+        Shipment(id="S1", distributor_id="A", origin="X", destination="Y", weight=Decimal("8"), volume=Decimal("8")),
+        Shipment(id="S2", distributor_id="B", origin="X", destination="Y", weight=Decimal("5"), volume=Decimal("5")),
+        Shipment(id="S3", distributor_id="C", origin="X", destination="Y", weight=Decimal("7"), volume=Decimal("7")),
+    ]
+
+
+def test_weight_proportional():
+    alloc = CostAllocator()
+    shares = alloc.allocate(_shipments(), Decimal("1200"), AllocationPolicy.WEIGHT_PROPORTIONAL)
+    assert shares["A"] == Decimal("480.00")
+    assert shares["B"] == Decimal("300.00")
+    assert shares["C"] == Decimal("420.00")
+    assert sum(shares.values()) == Decimal("1200.00")
+
+
+def test_volume_proportional():
+    alloc = CostAllocator()
+    shares = alloc.allocate(_shipments(), Decimal("1200"), AllocationPolicy.VOLUME_PROPORTIONAL)
+    assert shares["A"] == Decimal("480.00")
+    assert shares["B"] == Decimal("300.00")
+    assert shares["C"] == Decimal("420.00")
+
+
+def test_determinism():
+    alloc = CostAllocator()
+    s = _shipments()
+    a = alloc.allocate(s, Decimal("1200"), AllocationPolicy.WEIGHT_PROPORTIONAL)
+    b = alloc.allocate(s, Decimal("1200"), AllocationPolicy.WEIGHT_PROPORTIONAL)
+    assert a == b
